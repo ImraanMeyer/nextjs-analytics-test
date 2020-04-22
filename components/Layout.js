1fd448f67a785ac46be14a1bdcbe251cece2c1ea
ImraanMeyer <imraan.meyer97@gmail.com>
2020-04-22 14:17:45 +0200

    React GA

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,16 @@
 import Head from 'next/head';
 import Header from './Header';
+import ReactGA from 'react-ga';
+
+
 
 const Layout = (props) => (
+	ReactGA.initialize('UA-164284027-1');
+	ReactGA.pageview(window.location.pathname + window.location.search);
+
 	<div>
 		<Head>
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','UA-164209859-1%22%3E');`
-				}}
-			/>
-
-			<noscript
-				dangerouslySetInnerHTML={{
-					__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-164209859-1%22%3E" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`
-				}}
-			/>
+			
 
 			{/*bootstrap link*/}
 			<link
--- components/googleAnalytics.js
+++ components/googleAnalytics.js
@@ -1,48 +1,48 @@
-// googleAnalytics.js
+// // googleAnalytics.js
  
-import ReactGA from "react-ga"
+// import ReactGA from "react-ga"
  
-export const initGA = () => {
-  ReactGA.initialize("UA-164209859-1")
-}
+// export const initGA = () => {
+//   ReactGA.initialize("UA-164209859-1")
+// }
  
-export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
-}
+// export const logPageView = () => {
+//   ReactGA.set({ page: window.location.pathname })
+//   ReactGA.pageview(window.location.pathname)
+// }
 
-/*<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-164209859-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// /*<!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164209859-1"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
 
-  gtag('config', 'UA-164209859-1');
-</script>
-*/ 
+//   gtag('config', 'UA-164209859-1');
+// </script>
+// */ 
 
-import ReactGA from 'react-ga'
+// import ReactGA from 'react-ga'
 
-export const initGA = () => {
-    console.log('GA Init')
-    ReactGA.initialize('XXXXXXX')
-}
+// export const initGA = () => {
+//     console.log('GA Init')
+//     ReactGA.initialize('XXXXXXX')
+// }
 
-export const logPageView = () => {
-    console.log(`Logging pageview for ${window.location.pathname}`)
-    ReactGA.set({ page: window.location.pathname })
-    ReactGA.pageview(window.location.pathname)
-  }
+// export const logPageView = () => {
+//     console.log(`Logging pageview for ${window.location.pathname}`)
+//     ReactGA.set({ page: window.location.pathname })
+//     ReactGA.pageview(window.location.pathname)
+//   }
 
-  export const logEvent = (category = '', action = '') => {
-    if (category && action) {
-      ReactGA.event({ category, action })
-    }
-  }
+//   export const logEvent = (category = '', action = '') => {
+//     if (category && action) {
+//       ReactGA.event({ category, action })
+//     }
+//   }
   
-  export const logException = (description = '', fatal = false) => {
-    if (description) {
-      ReactGA.exception({ description, fatal })
-    }
-  }
+//   export const logException = (description = '', fatal = false) => {
+//     if (description) {
+//       ReactGA.exception({ description, fatal })
+//     }
+//   }
